a0bb4a9fafe318320af160ed3e9ecf97ef712a5c
Emmanuel Comba <emmanuel@comba.fr>
2019-03-20 14:57:05 +0100

    *AF-22096 DocFactory: new DocumentCreationOptions property (removeWhiteSpaceInPdfEditableFields) for removing the white spaces leaved after mail merge in an editable PDF. See also https://forum.aspose.com/t/fillable-word-docs-to-fillable-pdfs/15360 LicenseLoader stores internally the Aspose product licenses already loaded in a given Ivy project so that we don't need to load the license file unnecessarily. IvyAddOns POM: set the dependency to the filemanager api from 7.3.0-SNAPSHOT instead of 7.3

--- ivyProjects/DocFactory/src/ch/ivyteam/ivy/addons/docfactory/aspose/LicenseLoader.java
+++ ivyProjects/DocFactory/src/ch/ivyteam/ivy/addons/docfactory/aspose/LicenseLoader.java
@@ -1,10 +1,14 @@
 package ch.ivyteam.ivy.addons.docfactory.aspose;
 
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import ch.ivyteam.ivy.ThirdPartyLicenses;
 
-public class LicenseLoader {
+public final class LicenseLoader {
+	
+	private final static Map<AsposeProduct, Object> LOADED_ASPOSE_LICENSES = new HashMap<>();
 	
 	private LicenseLoader() {}
 	
@@ -14,25 +18,29 @@ public class LicenseLoader {
 	 * @throws Exception
 	 */
 	public static void loadLicenseforProduct(AsposeProduct product) throws Exception {
+		if(isLicenseForProductAlreadyLoaded(product)) {
+			return;
+		}
 		InputStream in = ThirdPartyLicenses.getDocumentFactoryLicense();
 		
 		switch (product) {
 		case WORDS:
-			new com.aspose.words.License().setLicense(in);
+			LOADED_ASPOSE_LICENSES.put(product, loadAsposeWordsLicense(in));
 			break;
 		case PDF:
-			new com.aspose.pdf.License().setLicense(in);
+			LOADED_ASPOSE_LICENSES.put(product, loadAsposePdfLicense(in));
 			break;
 		case CELLS:
-			new com.aspose.cells.License().setLicense(in);
+			LOADED_ASPOSE_LICENSES.put(product, loadAsposeCellsLicense(in));
 			break;
 		case SLIDES:
-			new com.aspose.slides.License().setLicense(in);
+			LOADED_ASPOSE_LICENSES.put(product, loadAsposeSlidesLicense(in));
 			break;	
 		default:
 			break;
 		}
 	}
+
 	
 	/**
 	 * Loads the Aspose License for the all the {@link #AsposeProduct}.
@@ -42,6 +50,78 @@ public class LicenseLoader {
 		for (AsposeProduct product : AsposeProduct.values()) {
 			loadLicenseforProduct(product);
 		}
+	}
+	
+	/**
+	 * returns true if the given license product has already been loaded, else false.
+	 * @param product the AsposeProduct which license load status should be checked
+	 * @return
+	 */
+	public static boolean isLicenseForProductAlreadyLoaded(AsposeProduct product) {
+		return LOADED_ASPOSE_LICENSES.containsKey(product);
+	}
+	
+	/**
+	 * Clears the internal loaded licenses store. 
+	 */
+	public static void clearLoadedLicenses() {
+		LOADED_ASPOSE_LICENSES.clear();
+	}
+	
+	/**
+	 * not public API
+	 * @param in
+	 * @return
+	 * @throws Exception
+	 */
+	private static com.aspose.words.License loadAsposeWordsLicense(InputStream in) throws Exception {
+		com.aspose.words.License lic = new com.aspose.words.License();
+		if(in != null) {
+			lic.setLicense(in);
+		}
+		return lic;
+	}
+	
+	/**
+	 * not public API
+	 * @param in
+	 * @return
+	 * @throws Exception
+	 */
+	private static com.aspose.cells.License loadAsposeCellsLicense(InputStream in) throws Exception {
+		com.aspose.cells.License lic = new com.aspose.cells.License();
+		if(in != null) {
+			lic.setLicense(in);
+		}
+		return lic;
+	}
+	
+	/**
+	 * not public API
+	 * @param in
+	 * @return
+	 * @throws Exception
+	 */
+	private static com.aspose.pdf.License loadAsposePdfLicense(InputStream in) throws Exception {
+		com.aspose.pdf.License lic = new com.aspose.pdf.License();
+		if(in != null) {
+			lic.setLicense(in);
+		}
+		return lic;
+	}
+	
+	/**
+	 * not public API
+	 * @param in
+	 * @return
+	 * @throws Exception
+	 */
+	private static com.aspose.slides.License loadAsposeSlidesLicense(InputStream in) throws Exception {
+		com.aspose.slides.License lic = new com.aspose.slides.License();
+		if(in != null) {
+			lic.setLicense(in);
+		}
+		return lic;
 	}
 
 }
