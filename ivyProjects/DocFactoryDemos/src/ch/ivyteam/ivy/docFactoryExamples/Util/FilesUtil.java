a1cf9e997c9b13fb46a7591ab9d6a991793b82a2
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-10-29 14:28:53 +0100

    XIVY-3334 fix: files to app dir
    - to valid form on all environments (windows, linux, engine, designer)
    
    Conflicts solved:
    	ivyProjects/DocFactoryDemos/src/ch/ivyteam/ivy/docFactoryExamples/Util/FilesUtil.java

--- ivyProjects/DocFactoryDemos/src/ch/ivyteam/ivy/docFactoryExamples/Util/FilesUtil.java
+++ ivyProjects/DocFactoryDemos/src/ch/ivyteam/ivy/docFactoryExamples/Util/FilesUtil.java
@@ -5,25 +5,24 @@ import java.io.IOException;
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.scripting.objects.File;
 
-public class FilesUtil {
+public class FilesUtil
+{
+  /*
+   * set file reference into the session
+   */
+  public static void setFileRef(File ivyFile)
+  {
+    Ivy.session().setAttribute("docRef", Ivy.html().fileref(ivyFile));
+    Ivy.session().setAttribute("docFilename", ivyFile.getName());
+  }
 
-	/*
-	 * set file reference into the session
-	*/
-	public static void setFileRef(File ivyFile)
-	{
-		Ivy.session().setAttribute("docRef", Ivy.html().fileref(ivyFile));
-		Ivy.session().setAttribute("docFilename", ivyFile.getName());
-
-	}
-
-	/*
-	 * set file reference into the session
-	 */
-	public static void setFileRef(java.io.File file) throws IOException
-	{
-		Ivy.session().setAttribute("docRef", Ivy.html().fileref(new File(file.getParentFile().getName()+"/"+file.getName())));
-		Ivy.session().setAttribute("docFilename", file.getName());
-	}		
+  /*
+   * set file reference into the session
+   */
+  public static void setFileRef(java.io.File file) throws IOException
+  {
+    File ivyFile = new File(file.getParentFile().getName() + "/" + file.getName(), false);
+    setFileRef(ivyFile);
+  }
 
 }
