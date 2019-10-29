34686d53a9d38e1d96a6312cd8f6f73cd099b41e
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-10-29 21:11:40 +0100

    XIVY-3334 refactor: auto-format

--- ivyProjects/DocFactoryDemos/src/ch/ivyteam/ivy/docFactoryExamples/SimpleDocumentCreator.java
+++ ivyProjects/DocFactoryDemos/src/ch/ivyteam/ivy/docFactoryExamples/SimpleDocumentCreator.java
@@ -10,60 +10,68 @@ import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.workflow.document.IDocument;
 import ch.ivyteam.ivy.workflow.document.Path;
 
-public class SimpleDocumentCreator {
-	private String name;
-	private Long documentId;
+public class SimpleDocumentCreator
+{
+  private String name;
+  private Long documentId;
 
-	public void init() {
-		// Load license for aspose slides & cell
-		try {
-			LicenseLoader.loadLicenseforProduct(AsposeProduct.WORDS);
-		} catch (Exception e) {
-			Ivy.log().error(e);
-		}
-	}
+  public void init()
+  {
+    // Load license for aspose slides & cell
+    try
+    {
+      LicenseLoader.loadLicenseforProduct(AsposeProduct.WORDS);
+    }
+    catch (Exception e)
+    {
+      Ivy.log().error(e);
+    }
+  }
 
-	public void createHelloWord() throws Exception {
+  public void createHelloWord() throws Exception
+  {
 
-		String bindingName = name.trim().replace(" ", "_");
+    String bindingName = name.trim().replace(" ", "_");
 
-		Document doc = null;
-		ch.ivyteam.ivy.scripting.objects.File tempFileIvy = 
-		        new ch.ivyteam.ivy.scripting.objects.File("Hello_" + bindingName + ".pdf", true);
+    ch.ivyteam.ivy.scripting.objects.File tempFileIvy = new ch.ivyteam.ivy.scripting.objects.File(
+            "Hello_" + bindingName + ".pdf", true);
 
-		doc = new Document();
-		// Write some text
-		DocumentBuilder builder = new DocumentBuilder(doc);
-		builder.writeln("Hello " + name);
-		builder.writeln("Nice to meet you :)");
+    Document doc = new Document();
+    DocumentBuilder builder = new DocumentBuilder(doc);
+    // Write some text
+    builder.writeln("Hello " + name);
+    builder.writeln("Nice to meet you :)");
 
-		// Save to ivy temporary file
-		doc.save(tempFileIvy.getAbsolutePath(), SaveFormat.PDF);
+    // Save to ivy temporary file
+    doc.save(tempFileIvy.getAbsolutePath(), SaveFormat.PDF);
 
-		// Save to ivy database
-		IDocument document = Ivy.wfCase().documents()
-										.add(new Path("KindOfDocument/" + tempFileIvy.getName()))
-										.write()
-										.withContentFrom(tempFileIvy.getJavaFile());
-		
-		// Get document id for the next dialog
-		documentId = document.getId();
-	}
+    // Save to ivy database
+    IDocument document = Ivy.wfCase().documents()
+            .add(new Path("KindOfDocument/" + tempFileIvy.getName()))
+            .write()
+            .withContentFrom(tempFileIvy.getJavaFile());
 
-	public String getName() {
-		return name;
-	}
+    // Get document id for the next dialog
+    documentId = document.getId();
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public String getName()
+  {
+    return name;
+  }
 
-	public Long getDocumentId() {
-		return documentId;
-	}
+  public void setName(String name)
+  {
+    this.name = name;
+  }
 
-	public void setDocumentId(Long documentId) {
-		this.documentId = documentId;
-	}
+  public Long getDocumentId()
+  {
+    return documentId;
+  }
 
+  public void setDocumentId(Long documentId)
+  {
+    this.documentId = documentId;
+  }
 }
