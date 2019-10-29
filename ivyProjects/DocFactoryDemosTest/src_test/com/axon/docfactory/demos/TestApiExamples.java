2aa77c20408f85aa346debc5d0aec0405fbfcfd3
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-10-29 10:25:26 +0100

    XIVY-3334 refactor: isolate re-usable test infra
    - shared test infrastructure for similar http client test.

--- ivyProjects/DocFactoryDemosTest/src_test/com/axon/docfactory/demos/TestApiExamples.java
+++ ivyProjects/DocFactoryDemosTest/src_test/com/axon/docfactory/demos/TestApiExamples.java
@@ -1,99 +1,57 @@
 package com.axon.docfactory.demos;
 
+import static com.axon.test.DocResponse.contentType;
+import static com.axon.test.DocResponse.getDocLink;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.net.CookieManager;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpClient.Redirect;
-import java.net.http.HttpRequest;
-import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 
-import javax.ws.rs.core.HttpHeaders;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.Test;
 
+import com.axon.test.DocType;
 import com.axon.test.EngineUrl;
+import com.axon.test.SimpleHttpClient;
 
 public class TestApiExamples
 {
-  private final HttpClient client = HttpClient.newBuilder()
-          .followRedirects(Redirect.NORMAL)
-          .cookieHandler(new CookieManager())
-          .build();
-  
   private final String docFactoryApiExamples = EngineUrl.process()+"/DocFactoryDemos/16B45CBCE0D2056C";
 
+  private final SimpleHttpClient http = new SimpleHttpClient();
+  
   @Test
   public void docWithCompositeObj() throws Exception
   {
-    HttpResponse<String> response = request(docFactoryApiExamples+"/start6.ivp");
+    HttpResponse<String> response = http.request(docFactoryApiExamples+"/start6.ivp");
     String docLink = getDocLink(response);
     assertThat(docLink).endsWith("DocWithCompositeObject.pdf");
-    assertThat(contentType(checkFile(docLink))).isEqualTo("application/pdf");
+    assertThat(contentType(http.head(docLink))).isEqualTo(DocType.PDF);
   }
   
   @Test
   public void docWithNestedTablesPDF() throws Exception
   {
-    HttpResponse<String> response = request(docFactoryApiExamples+"/start3.ivp");
+    HttpResponse<String> response = http.request(docFactoryApiExamples+"/start3.ivp");
     String docLink = getDocLink(response);
     assertThat(docLink).endsWith("DocWithFullNestedTables.pdf");
-    assertThat(contentType(checkFile(docLink))).isEqualTo("application/pdf");
+    assertThat(contentType(http.head(docLink))).isEqualTo(DocType.PDF);
   }
   
   @Test
   public void docWithNestedTablesDOCX() throws Exception
   {
-    HttpResponse<String> response = request(docFactoryApiExamples+"/start4.ivp");
+    HttpResponse<String> response = http.request(docFactoryApiExamples+"/start4.ivp");
     String docLink = getDocLink(response);
     assertThat(docLink).endsWith("DocWithFullNestedTables.docx");
-    assertThat(contentType(checkFile(docLink)))
-      .isEqualTo("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+    assertThat(contentType(http.head(docLink)))
+      .isEqualTo(DocType.DOCX);
   }
   
   @Test
   public void docWithNestedTablesHTML() throws Exception
   {
-    HttpResponse<String> response = request(docFactoryApiExamples+"/start5.ivp");
+    HttpResponse<String> response = http.request(docFactoryApiExamples+"/start5.ivp");
     String docLink = getDocLink(response);
     assertThat(docLink).endsWith("DocWithFullNestedTables.html");
-    assertThat(contentType(checkFile(docLink))).isEqualTo("text/html");
+    assertThat(contentType(http.head(docLink))).isEqualTo("text/html");
   }
-
-  private static String getDocLink(HttpResponse<String> response) 
-  {
-    assertThat(response.statusCode())
-      .as(response.toString()+"\n"+response.body())
-      .isEqualTo(200);
-    Element result = Jsoup.parse(response.body()).getElementById("docLink");
-    return result.getElementsByTag("a").attr("href");
-  }
-  
-  private static String contentType(HttpResponse<?> response)
-  {
-    return response.headers().firstValue(HttpHeaders.CONTENT_TYPE).get();
-  }
-  
-  private HttpResponse<String> request(String uri) throws Exception
-  {
-    HttpRequest request = HttpRequest.newBuilder()
-      .uri(URI.create(uri))
-      .build();
-    return client.send(request, BodyHandlers.ofString());
-  }
-  
-  private HttpResponse<Void> checkFile(String fileLink) throws Exception
-  {
-    HttpRequest request = HttpRequest.newBuilder()
-      .uri(URI.create(fileLink))
-      .method("HEAD", BodyPublishers.noBody())
-      .build();
-    return client.send(request, BodyHandlers.discarding());
-  }
-  
 }
