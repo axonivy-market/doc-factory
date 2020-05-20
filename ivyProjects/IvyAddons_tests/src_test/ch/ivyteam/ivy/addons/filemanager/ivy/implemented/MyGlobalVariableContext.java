b49ec557303995b1fd3f78b555eb811a66790846
Lukas Lieb <lukas.lieb@axonivy.com>
2020-05-20 08:13:06 +0200

    remove unimplemented method comments
    Former-commit-id: b7945e2c460ae78995313f236e777b0806681fd0

--- ivyProjects/IvyAddons_tests/src_test/ch/ivyteam/ivy/addons/filemanager/ivy/implemented/MyGlobalVariableContext.java
+++ ivyProjects/IvyAddons_tests/src_test/ch/ivyteam/ivy/addons/filemanager/ivy/implemented/MyGlobalVariableContext.java
@@ -7,50 +7,45 @@ import ch.ivyteam.ivy.globalvars.IGlobalVariableContext;
 
 public class MyGlobalVariableContext implements IGlobalVariableContext {
 
-	@Override
-	public String get(String varName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public String get(String varName)
+  {
+    return null;
+  }
 
-	@Override
-	public Set<String> getVariableNames() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public Set<String> getVariableNames()
+  {
+    return null;
+  }
 
   @Override
   public String set(String name, String value)
   {
-    // TODO Auto-generated method stub
     return null;
   }
 
   @Override
   public String reset(String name)
   {
-    // TODO Auto-generated method stub
     return null;
   }
 
   @Override
   public String getForEnvironment(String environment, String name)
   {
-    // TODO Auto-generated method stub
     return null;
   }
 
   @Override
   public String setForEnvironment(String environment, String name, String value)
   {
-    // TODO Auto-generated method stub
     return null;
   }
 
   @Override
   public String resetForEnvironment(String environment, String name)
   {
-    // TODO Auto-generated method stub
     return null;
   }
 
