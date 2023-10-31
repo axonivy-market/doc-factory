c7010743897eb4256706b892d99c6f25ef1fd65e
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-10-31 15:07:14 +0100

    mark integration tests as such

--- aspose-barcode-demo-test/src_test/com/axonivy/utils/docfactory/test/WebTestAsposeBarcodeDemoIT.java
+++ aspose-barcode-demo-test/src_test/com/axonivy/utils/docfactory/test/WebTestAsposeBarcodeDemoIT.java
@@ -12,21 +12,20 @@ import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.ivy.webtest.engine.EngineUrl;
 
 @IvyWebTest(headless = true)
-public class WebTestAsposeBarcodeDemo{
-	
+public class WebTestAsposeBarcodeDemoIT{
+
   private static final String BARCODE_STRING = "form:str";
   private static final String PROCEED_BUTTON = "form:proceed";
   private static final String BARCODE_IMAGE = "code_128";
-  
+
   @Test
   public void createBarcodePage(){
-    
-	open(EngineUrl.createProcessUrl("AsposeBarcodeDemo/16CD7829EF6B489B/start.ivp"));
-    
+    open(EngineUrl.createProcessUrl("AsposeBarcodeDemo/16CD7829EF6B489B/start.ivp"));
+
     $(By.id(BARCODE_STRING)).shouldBe(empty);
     $(By.id(BARCODE_STRING)).sendKeys(String.valueOf("Hello Axon Ivy"));
     $(By.id(PROCEED_BUTTON)).shouldBe(visible).click();
-    
+
     $(By.id(BARCODE_IMAGE)).shouldBe(visible).isImage();
   }
 
