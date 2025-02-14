00a8151419df34769e540da597120d55bc982a43
Thuy Nguyen <145430420+nntthuy-axonivy@users.noreply.github.com>
2025-02-14 10:13:57 +0700

    MARP-1954 Resolve new dependabot dependencies (#298)

--- doc-factory-test/src_test/ch/ivyteam/ivy/addons/docfactory/pdfbox/PdfAValidator.java
+++ doc-factory-test/src_test/ch/ivyteam/ivy/addons/docfactory/pdfbox/PdfAValidator.java
@@ -1,36 +1,22 @@
 package ch.ivyteam.ivy.addons.docfactory.pdfbox;
 
+import java.io.File;
 import java.io.IOException;
 
-import org.apache.pdfbox.preflight.PreflightDocument;
+import org.apache.pdfbox.preflight.ValidationResult;
 import org.apache.pdfbox.preflight.parser.PreflightParser;
 
 public class PdfAValidator {
 
   /**
    * This method checks the validity of the PDF/A files.
-   * https://pdfbox.apache.org/1.8/cookbook/pdfavalidation.html
    * @param pdf the file to validate
-   * @return true if pdf box can validate the given file. Else false.
+   * @return true if PDF box can validate the given file. Else false.
    * @throws IOException
    */
-  public static boolean isPDFACompliant(java.io.File pdf) throws IOException {
-    boolean isValid = false;
-    PreflightDocument document = null;
-    try {
-      PreflightParser parser = new PreflightParser(pdf);
-      parser.parse();
-      document = parser.getPreflightDocument();
-      document.validate();
-      isValid = document.getResult().isValid();
-    } catch (IOException e) {
-
-    } finally {
-      if (document != null) {
-        document.close();
-      }
-    }
-    return isValid;
+  public static boolean isPDFACompliant(File pdf) throws IOException {
+    ValidationResult validationResult = PreflightParser.validate(pdf);
+    return validationResult.isValid();
   }
 
 }
