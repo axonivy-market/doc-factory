e77a78d14268bee895ae5f29236f150ad9132a5e
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-07-30 22:26:50 +0700

    MARP-2641 test

--- doc-factory-demos/src/ch/ivyteam/ivy/docFactoryExamples/DocumentPreviewBean.java
+++ doc-factory-demos/src/ch/ivyteam/ivy/docFactoryExamples/DocumentPreviewBean.java
@@ -2,13 +2,15 @@ package ch.ivyteam.ivy.docFactoryExamples;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
 import javax.faces.view.ViewScoped;
 
 import org.apache.commons.lang3.ObjectUtils;
@@ -16,24 +18,34 @@ import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 import com.aspose.pdf.Document;
+
 import ch.ivyteam.ivy.addons.docfactory.entity.DocumentPreview;
 import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.persistence.PersistencyException;
 import ch.ivyteam.ivy.process.call.SubProcessCall;
 import ch.ivyteam.ivy.process.call.SubProcessCallResult;
+import ch.ivyteam.ivy.scripting.objects.File;
+import ch.ivyteam.ivy.security.exec.Sudo;
+import ch.ivyteam.ivy.workflow.ICase;
+import ch.ivyteam.ivy.workflow.document.IDocument;
+import ch.ivyteam.ivy.workflow.document.IDocumentService;
 
-@ManagedBean
-@SessionScoped
-public class DocumentPreviewBean implements Serializable {
+// @ManagedBean
+// @ViewScoped
+public class DocumentPreviewBean {
 
-  private static final long serialVersionUID = 1L;
+  // private static final long serialVersionUID = 1L;
+  private List<IDocument> uploadedFiles = new ArrayList<>();
   private StreamedContent streamedContent;
   private boolean isImage;
 
-  @PostConstruct
-  public void init() {
-    isImage = false;
-    streamedContent = createEmptyPdfStreamedContent();
-  }
+  // @PostConstruct
+  // public void init() {
+  // uploadedFiles = new ArrayList<>();
+  // isImage = false;
+  // streamedContent = createEmptyPdfStreamedContent();
+  // Ivy.log().warn("sadasddasads");
+  // }
 
   public void handleFileUpload(FileUploadEvent event) throws Exception {
     var entity = new DocumentPreview(event);
@@ -42,6 +54,73 @@ public class DocumentPreviewBean implements Serializable {
     isImage = entity.getContentType() != null && entity.getContentType().startsWith("image/");
     streamedContent = (StreamedContent) callResult.get("streamedContent");
     Ivy.log().warn(streamedContent.getName());
+  }
+
+  public static IDocument handleFileUpload2(FileUploadEvent event) throws IOException {
+    IDocument document = upload(event.getFile().getFileName(), event.getFile().getInputStream());
+    Ivy.log().warn(document.getName());
+    Ivy.log().warn(document.getId());
+    return document;
+
+  }
+
+  public static StreamedContent reviewFile(IDocument document) {
+    InputStream inputStream = documentsOf(Ivy.wfCase()).get(Long.valueOf(document.getId())).read().asStream();
+    return DefaultStreamedContent.builder().name("empty.pdf").contentType("application/pdf").stream(() -> inputStream)
+        .build();
+  }
+
+  // public static StreamedContent reviewFile(IDocument document) throws IOException {
+  // InputStream inputStream = documentsOf(Ivy.wfCase()).get(Long.valueOf(document.getId())).read().asStream();
+  // File file = new File(document.getPath().asString());
+  // var entity =
+  // new DocumentPreview(document.getName(), Files.probeContentType(file.getJavaFile().toPath()), inputStream);
+  // SubProcessCallResult callResult = SubProcessCall.withPath("Functional Processes/reviewDocument")
+  // .withStartName("reviewDocument").withParam("documentReview", entity).call();
+  // return (StreamedContent) callResult.get("streamedContent");
+  // }
+
+  // public StreamedContent download(IvyDocument document) {
+  // InputStream inputStream = documentsOf(iCase).get(Long.valueOf(document.getId())).read().asStream();
+  // return DefaultStreamedContent.builder().stream(() -> inputStream).contentType(document.getContentType())
+  // .name(document.getName()).build();
+  // }
+
+  // public IvyDocument transform(IDocument document) {
+  // try {
+  // File file = new File(document.getPath().asString());
+  // return IvyDocument.builder()
+  // .id(String.valueOf(document.getId()))
+  // .name(document.getName())
+  // .path(document.getPath().asString())
+  // .size(document.getSize())
+  // .creation(document.getCreation())
+  // .lastModification(document.getLastModification())
+  // .uuid(document.uuid())
+  // .contentType(Files.probeContentType(file.getJavaFile().toPath()))
+  // .create();
+  // } catch (IOException e) {
+  // Ivy.log().error(e);
+  // return new IvyDocument();
+  // }
+  // }
+
+  public static IDocument upload(String filename, InputStream content) {
+    try {
+      return documentsOf(Ivy.wfCase()).add(filename).write().withContentFrom(content);
+    } catch (PersistencyException e) {
+      Ivy.log().error("Error in uploading the document {0} ", e, filename);
+      return null;
+    }
+  }
+
+  private static IDocumentService documentsOf(ICase iCase) {
+    try {
+      return Sudo.call(iCase::documents);
+    } catch (Exception e) {
+      return null;
+    }
+
   }
 
   public StreamedContent getStreamedContent() {
@@ -85,4 +164,14 @@ public class DocumentPreviewBean implements Serializable {
       return null;
     }
   }
+
+  public List<IDocument> getUploadedFiles() {
+    return uploadedFiles;
+  }
+
+  public void setUploadedFiles(List<IDocument> uploadedFiles) {
+    this.uploadedFiles = uploadedFiles;
+  }
+
+
 }
--- doc-factory/src/ch/ivyteam/ivy/addons/docfactory/entity/DocumentPreview.java
+++ doc-factory/src/ch/ivyteam/ivy/addons/docfactory/entity/DocumentPreview.java
@@ -1,5 +1,8 @@
 package ch.ivyteam.ivy.addons.docfactory.entity;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.file.UploadedFile;
 
@@ -7,6 +10,7 @@ public class DocumentPreview {
   private String fileName;
   private String contentType;
   private byte[] fileContent;
+  private InputStream inputStream;
 
   public DocumentPreview(String fileName, String contentType, byte[] fileContent) {
     this.fileName = fileName;
@@ -14,10 +18,18 @@ public class DocumentPreview {
     this.fileContent = fileContent;
   }
 
-  public DocumentPreview(FileUploadEvent event) {
+
+  public DocumentPreview(String fileName, String contentType, InputStream inputStream) {
+    this.fileName = fileName;
+    this.contentType = contentType;
+    this.inputStream = inputStream;
+  }
+
+  public DocumentPreview(FileUploadEvent event) throws IOException {
     this.fileName = event.getFile().getFileName();
     this.contentType = event.getFile().getContentType();
     this.fileContent = event.getFile().getContent();
+    this.inputStream = event.getFile().getInputStream();
   }
 
   public DocumentPreview(UploadedFile file) {
@@ -49,5 +61,16 @@ public class DocumentPreview {
   public void setFileContent(byte[] fileContent) {
     this.fileContent = fileContent;
   }
+
+
+  public InputStream getInputStream() {
+    return inputStream;
+  }
+
+
+  public void setInputStream(InputStream inputStream) {
+    this.inputStream = inputStream;
+  }
+
 
 }
