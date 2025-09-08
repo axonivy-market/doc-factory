7213c8abf1f224e72791939662d96c8a4fa925ed
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-09-08 11:03:24 +0700

    MARP-2848 Create new callable support for InputStream

--- doc-factory-demos/src/ch/ivyteam/ivy/docFactoryExamples/DemoDocumentPreviewService.java
+++ doc-factory-demos/src/ch/ivyteam/ivy/docFactoryExamples/DemoDocumentPreviewService.java
@@ -1,6 +1,9 @@
 package ch.ivyteam.ivy.docFactoryExamples;
 
+import static ch.ivyteam.ivy.addons.docfactory.DocFactoryConstants.PDF_CONTENT_TYPE;
+
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -30,12 +33,15 @@ public class DemoDocumentPreviewService {
   }
 
   public static StreamedContent previewDocument(IDocument document) throws IOException {
-    SubProcessCallResult callResult = 
-        SubProcessCall.withPath(PREVIEW_DOCUMENT_PROCESS_PATH)
-            .withStartName("previewDocument")
-            .withParam("file", document.read().asJavaFile())
-            .call();
-    return (StreamedContent) callResult.get(STREAMED_CONTENT);
+//    SubProcessCallResult callResult = 
+//        SubProcessCall.withPath(PREVIEW_DOCUMENT_PROCESS_PATH)
+//            .withStartName("previewDocument")
+//            .withParam("file", document.read().asJavaFile())
+//            .call();
+//    return (StreamedContent) callResult.get(STREAMED_CONTENT);
+    
+    InputStream test = previewDocumentViaInputStream(document.getName(), Files.probeContentType(document.read().asJavaFile().toPath()), document.read().asStream());
+    return convertOutputStreamToStreamedContent(document.getName(), PDF_CONTENT_TYPE, test.readAllBytes());
   }
 
   public static StreamedContent previewDocumentViaStreamContent(IDocument document) throws IOException {
@@ -46,6 +52,18 @@ public class DemoDocumentPreviewService {
             .call();
 
     return (StreamedContent) callResult.get(STREAMED_CONTENT);
+  }
+  
+  public static InputStream previewDocumentViaInputStream(String fileName, String contentType, InputStream inputStream) throws IOException {
+    SubProcessCallResult callResult =
+        SubProcessCall.withPath(PREVIEW_DOCUMENT_PROCESS_PATH)
+            .withStartName("previewDocumentByInputStream")
+            .withParam("fileName", fileName)
+            .withParam("contentType", contentType)
+            .withParam("inputStream", inputStream)
+            .call();
+
+    return (InputStream) callResult.get("inputStream");
   }
 
   public static IDocument upload(String filename, InputStream content) {
@@ -82,5 +100,14 @@ public class DemoDocumentPreviewService {
         }).build();
   }
 
- 
+  private static StreamedContent convertOutputStreamToStreamedContent(ByteArrayOutputStream pdfOut, String fileName) {
+    byte[] pdfBytes = pdfOut.toByteArray();
+    return convertOutputStreamToStreamedContent(fileName, PDF_CONTENT_TYPE, pdfBytes);
+  }
+
+  private static StreamedContent convertOutputStreamToStreamedContent(String fileName, String contentType,
+      byte[] fileContent) {
+    return DefaultStreamedContent.builder().contentType(contentType).name(fileName)
+        .stream(() -> new ByteArrayInputStream(fileContent)).build();
+  }
 }
